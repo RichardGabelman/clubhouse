df2a2bb85adf7381ac827132f03069fa061b57f1
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-03-21 19:25:04 -0700

    Add basic postgresql to create user and messages tables

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,8 +2,20 @@ const { Client } = require("pg");
 const { argv } = require("node:process");
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS users ();
-CREATE TABLE IF NOT EXISTS messages ();
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR (255) NOT NULL,
+  password VARCHAR (255) NOT NULL,
+  membership BOOL,
+  admin BOOL
+);
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  author_id INTEGER NOT NULL REFERENCES users(id),
+  title VARCHAR (255),
+  message VARCHAR (255),
+  time TIMESTAMP
+);
 `;
 
 async function main() {
