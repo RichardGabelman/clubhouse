5ec18b9d5e4250d0be43e3e7a9ffb0f5e779de31
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-04-11 15:37:21 -0700

    Add basic app.js items

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Clubhouse App - listening on port ${PORT}!`);
+});
